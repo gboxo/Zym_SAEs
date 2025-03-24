49a361dbe8ca032b0540f91f5e4826ebc5b4ffeb
gboxo <gerardboxocorominas@gmail.com>
2025-03-24 18:13:08 +0100

    com

---
+++ experiments/Sanity_Checks/feature_importance.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+"""
+We want to make sure that we can attribute the importance of a given feature in terms of activity prediction
+
+
+For that we will test different settings:
+- Logistic Regression
+    - With and without (positive restriction)
+    - For different thresholds
+- Linear Regression
+    - With and without (positive restriction)
+"""
+
+
+
+
