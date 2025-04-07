f90aeeec8dca870fab859b5ae76af922bd8d0d76
gboxo <gerardboxocorominas@gmail.com>
2025-04-07 17:51:59 +0200

    diffign sapi

--- experiments/diffing_sapi_07_04/gather_data.sh
+++ experiments/diffing_sapi_07_04/gather_data.sh
@@ -13,11 +13,11 @@ set -o pipefail
 
 
 # We convert the fasta files into a dataset (one for each iteration)
-#for i in $(seq 0 30);
-#do
-#    echo "Gathering data for iteration ${i}"
-#    python3 -m src.tools.data_utils.create_diffing_dataset --cfg_path experiments/diffing_sapi_07_04/create_diffing_dataset_cfg.yaml --iteration_num ${i}
-#    done
+for i in $(seq 0 30);
+do
+    echo "Gathering data for iteration ${i}"
+    python3 -m src.tools.data_utils.create_diffing_dataset --cfg_path experiments/diffing_sapi_07_04/create_diffing_dataset_cfg.yaml --iteration_num ${i}
+ done
 
 # We collect the pLDDT, activities, etc
 for i in $(seq 0 29);
